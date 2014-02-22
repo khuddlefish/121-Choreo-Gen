e77711bb482103f4e6599fcc9360c0f55f82e649
=Karen Huddleston <klh12012@pomona.edu>
2014-02-21 20:16:44 -0800

    Initial Commit

---
+++ ChoreoGen/XYZAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  XYZAppDelegate.h
+//  ChoreoGen
+//
+//  Created by Karen Huddleston on 2/21/14.
+//  Copyright (c) 2014 Karen Huddleston. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface XYZAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ ChoreoGen/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  ChoreoGen
+//
+//  Created by Karen Huddleston on 2/21/14.
+//  Copyright (c) 2014 Karen Huddleston. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "XYZAppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([XYZAppDelegate class]));
+    }
+}
