084647babe9eaae4924854fe00140d24e0d02adf
Noelle Fa-Kaji <noellekimi7@gmail.com>
2014-02-25 23:30:31 -0800

    Moved ChoreoGen files to one folder

---
+++ ChoreoGen/ChoreoGenTests/ChoreoGenTests.h
@@ -0,0 +1,13 @@
+//
+//  ChoreoGenTests.h
+//  ChoreoGenTests
+//
+//  Created by Karen Huddleston on 2/21/14.
+//  Copyright (c) 2014 Karen Huddleston. All rights reserved.
+//
+
+#import <SenTestingKit/SenTestingKit.h>
+
+@interface ChoreoGenTests : SenTestCase
+
+@end
---
+++ ChoreoGen/LatButtonsViewController.h
@@ -0,0 +1,13 @@
+//
+//  LatButtonsViewController.h
+//  ChoreoGen
+//
+//  Created by Karen Huddleston on 2/21/14.
+//  Copyright (c) 2014 Karen Huddleston. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface LatButtonsViewController : UIViewController
+
+@end
