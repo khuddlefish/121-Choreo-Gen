46f8adbf5ae10ab55435fe76460ec098cac26b34
Noelle Fa-Kaji <noellekimi7@gmail.com>
2014-02-25 23:05:15 -0800

    Silver dictionary still in progress -- moves arrays being implemented

---
+++ ChoreographyGenerator/ChoreographyGenerator/chaChaSilver.h
@@ -0,0 +1,14 @@
+//
+//  chaChaSilver.h
+//  ChoreographyGenerator
+//
+//  Created by Noelle Fa-Kaji on 2/23/14.
+//  Copyright (c) 2014 Noelle Fa-Kaji. All rights reserved.
+//
+
+#ifndef ChoreographyGenerator_chaChaSilver_h
+#define ChoreographyGenerator_chaChaSilver_h
+
+
+
+#endif
---
+++ ChoreographyGenerator/ChoreographyGenerator/chaChaSilver.m
@@ -0,0 +1,9 @@
+//
+//  chaChaSilver.c
+//  ChoreographyGenerator
+//
+//  Created by Noelle Fa-Kaji on 2/23/14.
+//  Copyright (c) 2014 Noelle Fa-Kaji. All rights reserved.
+//
+
+#include <stdio.h>
