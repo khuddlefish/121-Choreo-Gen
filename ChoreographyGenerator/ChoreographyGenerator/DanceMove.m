6dacc1cfb8748a7037d93d9beefbbfbe347caa3c
Latina Vidolova <l.vidolova@gmail.com>
2014-03-20 18:36:02 -0700

    Skeleton of database implemented mostly in comments and DanceMove class added. When we save routines to the database, it might be useful for a move to save it's idea? Basically we should think about how these moves that we're exporting from the database are going to re-enter later as routines.

---
+++ ChoreographyGenerator/ChoreographyGenerator/DanceMove.m
@@ -0,0 +1,13 @@
+//
+//  DanceMove.m
+//  ChoreographyGenerator
+//
+//  Created by Latina Vidolova on 3/20/14.
+//  Copyright (c) 2014 Noelle Fa-Kaji. All rights reserved.
+//
+
+#import "DanceMove.h"
+
+@implementation DanceMove
+
+@end
