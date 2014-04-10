9c7e09180d6c17b4934bb659d492cecebad78c20
Latina Vidolova <l.vidolova@gmail.com>
2014-04-10 09:47:18 -0700

    Move description view implemented.

---
+++ ChoreographyGenerator/ChoreographyGenerator/ChoreographyGenerator/MoveDetailsViewController.h
@@ -0,0 +1,13 @@
+//
+//  MoveDetailsViewController.h
+//  ChoreographyGenerator
+//
+//  Created by Latina Vidolova on 4/7/14.
+//  Copyright (c) 2014 Noelle Fa-Kaji. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MoveDetailsViewController : UIViewController
+
+@end
---
+++ ChoreographyGenerator/ChoreographyGenerator/ChoreographyGenerator/MoveDetailsViewController.m
@@ -0,0 +1,49 @@
+//
+//  MoveDetailsViewController.m
+//  ChoreographyGenerator
+//
+//  Created by Latina Vidolova on 4/7/14.
+//  Copyright (c) 2014 Noelle Fa-Kaji. All rights reserved.
+//
+
+#import "MoveDetailsViewController.h"
+
+@interface MoveDetailsViewController ()
+
+@end
+
+@implementation MoveDetailsViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender
+{
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
